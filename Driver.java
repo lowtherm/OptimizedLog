cd880aac044f7c9d53e5369a50e4fef4b01d8f95
Lowther <lowtherm@vdi-sccubu-003.lancs.local>
2020-11-06 12:20:52 +0000

    changed from testharness to driver

---
+++ Driver.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class TestHarness
+{
+    public static void main(String[] args)
+    {
+        OptimizedLog<String> log = new OptimizedLog<>();
+        log.add("Hello world!");
+        log.add("Another entry...");
+        log.add("SPAM...");
+        log.add("SPAM...");
+        log.add("SPAM...");
+        System.out.println(log);
+    }
+}
